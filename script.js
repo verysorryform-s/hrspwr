fc2432ced5759faabc734205158da94b91e4d0d9
verysorryform-s <verysorryform@gmail.com>
2026-02-09 23:14:05 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -2,13 +2,13 @@
 const sections = document.querySelectorAll("section");
 
 function checkVisibility() {
-    const triggerBottom = window.innerHeight * 0.85;
-    sections.forEach(section => {
-        const sectionTop = section.getBoundingClientRect().top;
-        if (sectionTop < triggerBottom) {
-            section.classList.add("visible");
-        }
-    });
+  const triggerBottom = window.innerHeight * 0.85;
+  sections.forEach(section => {
+    const sectionTop = section.getBoundingClientRect().top;
+    if (sectionTop < triggerBottom) {
+      section.classList.add("visible");
+    }
+  });
 }
 
 window.addEventListener("scroll", checkVisibility);
